d29f758cc1315b2e632c9886ef730b390decefe4
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-28 22:09:50 +0300

    Add category and location foreign keys in image model.

--- pictures/models.py
+++ pictures/models.py
@@ -5,15 +5,24 @@ from django.forms import CharField
 # Create your models here.
 class Location (models.Model):
     name = models.CharField(max_length=60)
+
+    def __str__ (self):
+      return self.name
+
 class Categories (models.Model):
     name = models.CharField (max_length=60)
+
+    def __str__ (self):
+      return self.name
 
 class Image (models.Model):
   image = models.ImageField(upload_to = 'images/')
   name = models.CharField(max_length =60)
   description = models.TextField()
   uploadby = models.CharField(max_length= 60, default='admin')
-  # author = models.ForeignKey(Editor)
+  author = models.CharField(max_Length = 60)
   pub_date = models.DateTimeField(auto_now_add=True)
+  category = models.ForeignKey(Categories)
+  location = models.ForeignKey (Location)
   
 
