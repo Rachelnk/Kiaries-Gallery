5824395fb00639f15e29089166484245875d3b39
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-28 22:00:15 +0300

    Add location and image model.

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,15 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class location (models.Model):
+    name = models.CharField(max_length=60)
+class Image (models.Model):
+  image = models.ImageField(upload_to = 'images/')
+  name = models.CharField(max_length =60)
+  description = models.TextField()
+  uploadby = models.CharField(max_length= 60, default='admin')
+  # author = models.ForeignKey(Editor)
+  pub_date = models.DateTimeField(auto_now_add=True)
+  
+
