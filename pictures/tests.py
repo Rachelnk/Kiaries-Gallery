7c95ea404946e36d79660b12a3d192e92b9fa10c
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-29 14:50:51 +0300

    Create a test class for categories model.

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Categories, Location, Image
 
 # Create your tests here.
+class CategoriesTestClass(TestCase):
+
+    def setUp(self):
+        self.food = Categories(name = 'food')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.food)), Categories
+
+    def test_save_method(self):
+        self.ray.save_category()
+        categories = Categories.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def tearDown(self):
+        Categories.objects.all().delete()
+        
