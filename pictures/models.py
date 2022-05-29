a8b0866d84276f4074ef6cc147b499720b3dd08d
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-29 11:25:33 +0300

    Create save location function.

--- pictures/models.py
+++ pictures/models.py
@@ -6,6 +6,10 @@ from django.forms import CharField
 class Location (models.Model):
     name = models.CharField(max_length=60)
 
+    def save_location(self):
+        self.save()
+
+
     def __str__ (self):
       return self.name
 
