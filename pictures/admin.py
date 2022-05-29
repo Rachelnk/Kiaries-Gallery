e19a57902adc7a0e4fad26f9c96e5b21e1aa5bbb
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-29 15:17:35 +0300

    Create admin credentials and add models to admin page.

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categories, Image, Location
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Categories)
+admin.site.register(Location)
+
