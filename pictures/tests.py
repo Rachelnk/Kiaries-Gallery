c440cc5e9128afbbe76174e0f065d593813980c3
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-30 19:49:39 +0300

    Add delete category testcase.

--- pictures/tests.py
+++ pictures/tests.py
@@ -5,16 +5,21 @@ from .models import Categories, Location, Image
 class CategoriesTestClass(TestCase):
 
     def setUp(self):
-        self.food = Categories(name = 'food')
+        self.category = Categories(name = 'food')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.food)), Categories
 
     def test_save_method(self):
-        self.ray.save_category()
+        self.category.save_category()
         categories = Categories.objects.all()
         self.assertTrue(len(categories) > 0)
 
     def tearDown(self):
         Categories.objects.all().delete()
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Categories.objects.all()
+        self.assertTrue(len(category) == 0)
         
