6a3f50c674f3fbacf063302ba74f10137cfa58ab
Rachel Kiarie <rachelnnk94@gmail.com>
2022-05-30 20:11:52 +0300

    Add test cases for image model.

--- pictures/tests.py
+++ pictures/tests.py
@@ -20,7 +20,7 @@ class CategoriesTestClass(TestCase):
         category = Categories.objects.all()
         self.assertTrue(len(category) == 0)
 
-class LocationTestCase(TestCase):
+class LocationTestClass(TestCase):
     def setUp(self):
         self.category = Categories(name = 'Nairobi')
 
@@ -47,6 +47,54 @@ class LocationTestCase(TestCase):
         self.location.update_location(self.location.id, new_location)
         changed_location = Location.objects.filter(name='Nairobi')
         self.assertTrue(len(changed_location) > 0)
+
+
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='Nairobi')
+        self.location.save_location()
+
+        self.category = Categories(name='food')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='image', description='test image', location=self.location,
+                                category=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.image_test.id, 'photos/test.jpg')
+        changed_img = Image.objects.filter(image='photos/test.jpg')
+        self.assertTrue(len(changed_img) > 0)
+
+    def test_get_image_by_id(self):
+        found_image = self.image_test.get_image_by_id(self.image_test.id)
+        image = Image.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_image, image)
+
+    def test_search_image_by_location(self):
+        self.image_test.save_image()
+        found_images = self.image_test.filter_by_location(location='moringa')
+        self.assertTrue(len(found_images) == 1)
+
+    def test_search_image_by_category(self):
+        category = 'food'
+        found_img = self.image_test.search_by_category(category)
+        self.assertTrue(len(found_img) > 1)
+
+
 
 
     def tearDown(self):
